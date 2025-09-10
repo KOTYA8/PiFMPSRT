62c3f79421b6a9e4708d549eedaaf3a73bce11df
kOt <138892762+KOTYA8@users.noreply.github.com>
2025-09-10 22:20:50 +0300

    Update parser.py

--- pifmpsrtv5/parser.py
+++ pifmpsrtv5/parser.py
@@ -1,15 +1,20 @@
 def parse_line(line: str):
     """
-    Пример строки: s|thisistexthello|5/4
-    Возвращает: mode, text, times
+    Возможные форматы строк:
+      text
+      mode|text
+      mode|text|5/4
     """
+    if "|" not in line:
+        return None, line, [5]  # просто текст
+    
     parts = line.split("|")
     if len(parts) == 3:
         mode, text, times = parts
         times = [int(x) for x in times.split("/")]
     elif len(parts) == 2:
         mode, text = parts
-        times = [5]  # по умолчанию 5 секунд
+        times = [5]
     else:
         raise ValueError(f"Неправильный формат строки: {line}")
     return mode, text, times
