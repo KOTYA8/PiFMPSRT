dbdcc7fcbde7d1fe17bc30c311982bd2b4373f5a
kOt <138892762+KOTYA8@users.noreply.github.com>
2025-09-10 22:21:04 +0300

    Update ps_modes.py

--- pifmpsrtv5/ps_modes.py
+++ pifmpsrtv5/ps_modes.py
@@ -1,24 +1,24 @@
-from utils import pad_to_8, center_to_8
+from pifmpsrt.utils import pad_to_8, center_to_8
 
 def generate_ps_frames(mode, text, times=None):
     frames = []
 
-    if mode == "rs":  # обычный скролл (справа налево)
+    if mode == "rs":  # справа налево
         ext = text + "_" * 7
         for i in range(len(ext) - 7):
             frames.append(pad_to_8(ext[i:i+8]))
 
-    elif mode == "ls":  # левый скролл
-        ext = "_" * 7 + text
+    elif mode == "ls":  # слева направо
+        ext = text + "_" * 7
         for i in range(len(ext) - 7):
             frames.append(pad_to_8(ext[i:i+8]))
 
-    elif mode == "cs":  # появление из центра
+    elif mode == "cs":  # появление из центра (просто пример)
         padded = center_to_8(text)
         for i in range(len(padded)):
             frames.append(pad_to_8(padded[i:i+8]))
 
-    elif mode == "cst":  # центральный перенос (постепенный выход)
+    elif mode == "cst":  # центральный перенос
         ext = center_to_8(text) + "_" * 8
         for i in range(len(ext) - 7):
             frames.append(pad_to_8(ext[i:i+8]))
@@ -28,7 +28,7 @@ def generate_ps_frames(mode, text, times=None):
         for i in range(len(ext) - 7):
             frames.append(pad_to_8(ext[i:i+8]))
 
-    else:  # просто показать как есть
+    else:  # без режима → показать как есть
         frames.append(pad_to_8(text))
 
     return frames
